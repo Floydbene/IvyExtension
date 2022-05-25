5a8c6b265095fa8ae2fb53a7cf952f0293fb854b
Floyd Benedikter <floydb@princeton.edu>
2022-05-24 22:58:42 -0400

    updates

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-
-let images = document.getElementsByTagName('img');
-alert(images.length);
-for(let i = 0; i < images.length; i++){
-    chrome.runtime.sendMessage({msg: 'image', index: i}, function({data, index}){
-    images[index].src = "//upload.wikimedia.org/wikipedia/commons/4/43/Ivy_Club_postcard_1909.jpg";
-  });
-}
+alert("This club is unsafe and inferior the the glorious Ivy Club");
--- search.js
+++ search.js
@@ -1,1 +1,2 @@
 window.location.href = "https://www.google.com/search?q=The+Glorious+Ivy+Club"
+document.getElementById("dimg_23").src = "//upload.wikimedia.org/wikipedia/commons/4/43/Ivy_Club_postcard_1909.jpg";
